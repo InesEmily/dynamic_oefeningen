3a8d5c6803cda4a0f8486c23eea78780034c401a
ines <r0306129@student.thomasmore.be>
2022-10-04 10:29:41 +0200

    Bundel 4 tot en met 2.6 en 2.7

--- src/components/EventDemo.js
+++ src/components/EventDemo.js
@@ -9,7 +9,12 @@ export function EventDemo() {
             <div>
             <Button style={{marginTop: '5px'}} variant={"primary"} onClick={() => alert('bootstrap button is clicked')}>Click me please!</Button>
             </div>
+            <div>
             <button onClick={(e)=>alert(`hallo ${e.clientX},${e.clientY}`)}>klik</button>
+            </div>
+            <div style={{backgroundColor:'pink', padding:'15px', marginTop: '5px', marginBottom:'10px'}} onClick={()=>alert('dit is een div')}><p>Dit is een div met een eventhandler</p><a href={'#'} onClick={(e)=>{e.preventDefault();e.stopPropagation();{alert('button is clicked')}}}> dit een link met een href</a> - en - <Button variant={"primary"} onClick={()=>alert('click')}>dit is een button</Button>
+
+            </div>
         </>)
 
 }
