403a2cf5a021751b364897c350a6480f2864c0dd
ines <r0306129@student.thomasmore.be>
2022-10-04 11:28:04 +0200

    Bundel 4 oef 3.5

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -7,6 +7,7 @@ export function StateDemo(){
     const [demoValue,setDemoValue] =useState(0);
     return <Section title='state'>
         <div>de waarde van demoValue is {demoValue}</div>
+        <div>de waarde van demoValue is {demoValue}</div>
         <MyButton onClick={()=>setDemoValue(77)}>Set77</MyButton>
         <MyButton onClick={()=>setDemoValue(0)}>Set0</MyButton>
         <MyButton onClick={()=>setDemoValue(184)}>Set184</MyButton>
