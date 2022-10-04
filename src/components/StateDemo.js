b573d65099d70d98bb72c500bc226480676412c8
ines <r0306129@student.thomasmore.be>
2022-10-04 11:13:07 +0200

    Bundel 4 oef 3.3

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -6,7 +6,9 @@ export function StateDemo(){
     const [demoValue,setDemoValue] =useState(0);
     return <Section title='state'>
         <div>de waarde van demoValue is {demoValue}</div>
-        <Button variant='primary' size='sm' className='ms- mt-1' onClick={()=>setDemoValue(77)}>SET77</Button>
+        <span><Button variant='primary' size='sm' className='ms- mt-1' onClick={()=>setDemoValue(77)}>SET77</Button></span>
+        <span> <Button variant='primary' size='sm' className='ms- mt-1' onClick={()=>setDemoValue(0)}>SET0</Button></span>
+        <span><Button variant='primary' size='sm' className='ms- mt-1' onClick={()=>setDemoValue(184)}>SET184</Button></span>
     </Section>;
 
 }
