fa7a3acbbebd240b110b66b5e48e7cde68524797
ines <r0306129@student.thomasmore.be>
2022-10-11 09:53:59 +0200

    Bundel 4 oef 4.4

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,25 @@
 import {Button, Container, Row} from "react-bootstrap";
+import {useState} from "react";
+import {MyButton} from "./MyButton";
 
 export function Section(props) {
     const {title, children} = props;
+    const [isOpen,setIsOpen] =useState(false);
     return (
+
         <div className="mt-3 rounded shadow-sm" style={{backgroundColor: "lavender"}}>
             {/*<Button style={{margin:" 5px"}} variant={"primary"} onClick={()=>alert(button)}>Click</Button>*/}
             <h2 className="text-center">{title}</h2>
-            <Container>
-                <Row>
-                    {children}
-                </Row>
-            </Container>
+
+            <div>
+                <MyButton onClick={()=>{setIsOpen(!isOpen)}}>{isOpen? '-':'+'}</MyButton>
+                <Container>
+                    <Row>
+                        {(isOpen)?children:""}
+                    </Row>
+                </Container>
+
+            </div>
         </div>
     );
 }
